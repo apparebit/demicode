cf9d061e5f01f027af22f1e62d830b56a687fd53
Robert Grimm <apparebit@gmail.com>
2023-08-08 04:29:15 -0400

    Make several usability improvements
    * Group command line options so they are easier to learn
    * Add options to update UCD path and version
    * Add --with-arrow and --with-dash options for arrows and dashes
    * Add option to print number of code points in various pre-configured groups (which helped identify a bug)
    * Switch to using renderer

--- demicode/__main__.py
+++ demicode/__main__.py
@@ -1,25 +1,31 @@
 import sys
 
-from .ansi import Ansi
-
 if sys.version_info < (3, 11, 0):
     version = '.'.join(str(v) for v in sys.version_info[:3])
-    print(f'{Ansi.ERROR}Error: You are trying to run demicode with Python {version},')
+    print(f'Error: You are trying to run demicode with Python {version},')
     print(f'but demicode requires at least Python 3.11.0 or later')
-    print(f'Please try again after upgrading your Python installation.{Ansi.RESET}')
+    print(f'Please try again after upgrading your Python installation.')
     sys.exit(1)
 
 import argparse
 from collections.abc import Iterable, Sequence
 import itertools
 import os
+from pathlib import Path
 import re
 import sys
 from textwrap import dedent
 
 from .codepoint import CodePoint
 from .ucd import UCD
-from .display import page_lines, format_grid_lines, format_legend, format_lines
+from .display import (
+    add_presentation,
+    format_grid_lines,
+    format_legend,
+    format_lines,
+    page_lines,
+)
+from .render import Mode, Renderer
 from demicode import __version__
 
 
@@ -33,7 +39,7 @@ def width_limited_formatter(prog: str) -> argparse.HelpFormatter:
 def configure_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog='demicode',
-        description='        "Unicode is boring without hemi-semi-demicode!"',
+        description="""        "It's not just Unicode, it's hemi-semi-demicode!\"""",
         epilog=dedent("""
             Demicode's output is paged. Press <return> to advance to the next
             page. If you enter "q" or "quit" before the <return>, demicode
@@ -47,70 +53,150 @@ def configure_parser() -> argparse.ArgumentParser:
         """),
         formatter_class=width_limited_formatter,
     )
-    parser.add_argument(
+
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+    ucd_group = parser.add_argument_group('configure UCD')
+    ucd_group.add_argument(
+        '--ucd-path',
+        help='set path for local mirror of UCD',
+    )
+    ucd_group.add_argument(
+        '--ucd-version',
+        help='set UCD version from 4.1 onwards',
+    )
+
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+    cp_group = parser.add_argument_group('select code points')
+    cp_group.add_argument(
+        '--with-arrow',
+        action='store_true',
+        help='include codepoints for regular and long arrows',
+    )
+    cp_group.add_argument(
+        '--with-dash',
+        action='store_true',
+        help='include codepoints with Unicode\'s Dash property',
+    )
+    cp_group.add_argument(
         '--with-keycap',
         action='store_true',
-        help='include codepoints that combine with U+20E3\nfor keycaps.'
+        help='include codepoints that combine with U+20E3\nfor keycaps'
     )
-    parser.add_argument(
+    cp_group.add_argument(
         '--with-selection',
         action='store_true',
-        help='include curated selection of codepoints.'
+        help='include curated selection of codepoints'
     )
-    parser.add_argument(
+    cp_group.add_argument(
         '--with-variation',
         action='store_true',
-        help='include all codepoints that have text and\nemoji variations.'
+        help='include all codepoints that have text and\nemoji variations'
     )
-    parser.add_argument(
+    cp_group.add_argument(
         'codepoints',
         nargs='*',
         help="""include codepoints provided as space-
 separated hex numbers (4-6 digits, optionally
 prefixed with "U+") or as literal characters
-without intermediate spaces."""
+without intermediate spaces"""
     )
-    parser.add_argument(
+
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+    out_group = parser.add_argument_group('control output')
+    out_group.add_argument(
         '--as-grid',
         action='store_true',
-        help='display as grid without further UCD information.'
+        help='display as grid without further UCD information'
     )
-    parser.add_argument(
+    out_group.add_argument(
+        '--in-dark-mode',
+        action='store_true',
+        help='use colors suitable for dark mode'
+    )
+    out_group.add_argument(
         '--bright', '-b',
         default=0,
         action='count',
-        help='increase the color brightness of the output;\nmay be used more than once.'
+        help='use brighter colors in output;\nmay be repeated once'
     )
-    parser.add_argument(
+
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+    about_group = parser.add_argument_group('about this tool')
+    about_group.add_argument(
+        '--stats',
+        action='store_true',
+        help='display summary statistics and exit'
+    )
+    about_group.add_argument(
         '--version',
         action='store_true',
-        help='display the package version and exit.'
+        help='display the package version and exit'
     )
+
     return parser
 
 
-def main(arguments: Sequence[str]) -> int:
-    # Parse the options and handle version display (against pink background).
+# --------------------------------------------------------------------------------------
+
+
+def main(arguments: None | Sequence[str] = None) -> int:
+    # -------------------------- Parse the options and prepare console renderer
     parser = configure_parser()
+    arguments = arguments or sys.argv
     options = parser.parse_args(arguments[1:])
 
-    if options.version:
-        print(f'{Ansi.SGR("48;5;218;1")} demicode {__version__} {Ansi.Style.RESET}')
-        return 0
-
-    # Determine terminal's display settings.
     width, height = os.get_terminal_size()
     height -= 1
-    brightness = options.bright
+    renderer = Renderer(
+        width,
+        Mode.DARK if options.in_dark_mode else Mode.LIGHT,
+        options.bright
+    )
 
-    # Determine code points to display.
+    # ---------------------------------------------- Set UCD path and/or version
+    try:
+        if options.ucd_path:
+            UCD.use_path(Path(options.ucd_path))
+        if options.ucd_version:
+            UCD.use_version(options.ucd_version)
+    except ValueError as x:
+        print(renderer.error(f'Error: {str(x)}'))
+        return 1
+
+    # ------------------------------------------------ Handle version and stats
+    if options.version:
+        print(renderer.very_strong(f' demicode {__version__} '))
+        return 0
+
+    if options.stats:
+        print()
+        print(renderer.strong('Code Points with Given Property'))
+        print()
+        print(f'    dash         : {len(UCD.with_dash):4,d}')
+        print(f'    keycap       : {len(UCD.with_keycap):4,d}')
+        print(f'    noncharacter : {len(UCD.with_noncharacter):4,d}')
+        print(f'    selector     : {len(UCD.with_selector):4,d}')
+        print(f'    text v emoji : {len(UCD.with_variation):4,d}')
+        print(f'    white space  : {len(UCD.with_whitespace):4,d}')
+        print()
+        return 0
+
+    # ---------------------------------------- Determine code points to display
     codepoints: list[Iterable[CodePoint]] = []
+    if options.with_arrow:
+        codepoints.append(UCD.with_arrow)
+    if options.with_dash:
+        codepoints.append(sorted(UCD.with_dash))
     if options.with_keycap:
-        codepoints.append(UCD.with_keycap)
+        codepoints.append(sorted(UCD.with_keycap))
     if options.with_selection:
-        codepoints.append(UCD.with_selection)
+        codepoints.append(UCD.curated_selection)
     if options.with_variation:
-        codepoints.append(UCD.with_variation)
+        codepoints.append(sorted(UCD.with_variation))
     if options.codepoints:
         # Both hexadecimal numbers and characters are recognized.
         for argument in options.codepoints:
@@ -119,36 +205,34 @@ def main(arguments: Sequence[str]) -> int:
             else:
                 codepoints.append(CodePoint.of(ch) for ch in argument)
 
-    # If there's nothing to display, provide some hints.
+    # -------------------------------- If there's nothing to display, help user
     if len(codepoints) == 0:
-        print(f'{Ansi.ERROR}Error: There are no codepoints to show.')
-        print(f'Maybe try again with "1F49D" as argument——')
-        print(f'or with "-h" to see all options.{Ansi.RESET}')
+        print(renderer.error('Error: There are no codepoints to show.'))
+        print(renderer.error('Maybe try again with "1F49D" as argument——'))
+        print(renderer.error('or with "-h" to see all options.'))
         return 1
 
-    # Display the code points.
+    # ----------------------------------------------------- Display code points
     if options.as_grid:
         page_lines(
             height,
             None,
             format_grid_lines(
-                brightness,
-                width,
-                itertools.chain.from_iterable(codepoints)
+                renderer,
+                add_presentation(itertools.chain.from_iterable(codepoints)),
             )
         )
     else:
         page_lines(
             height,
-            format_legend(width),
+            format_legend(renderer),
             format_lines(
-                brightness,
-                width,
-                itertools.chain.from_iterable(codepoints)
+                renderer,
+                add_presentation(itertools.chain.from_iterable(codepoints)),
             )
         )
 
-    # Done.
+    # -------------------------------------------------------------------- Done
     return 0
 
 

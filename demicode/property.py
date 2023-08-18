7325a85737d32e35904ddf29487f14e1d5595cac
Robert Grimm <apparebit@gmail.com>
2023-08-18 09:58:14 -0400

    Rationalize support for UCD properties
    * BinaryProperty lists all supported binary properties
    * CharacterData groups a code point with all supported properties

--- demicode/property.py
+++ demicode/property.py
@@ -1,11 +1,19 @@
 """
-Python enumerations for Unicode properties.
+A code point's Unicode properties.
 """
 
+from dataclasses import dataclass
 from enum import StrEnum
+from .codepoint import CodePoint
 
 
 class Category(StrEnum):
+    """
+    The Unicode categories. Constant names are the property values in Unicode
+    notation, i.e., preserving capitalization. Constant values are the shorter
+    aliases.
+    """
+
     Uppercase_Letter = 'Lu'
     Lowercase_Letter = 'Ll'
     Titlecase_Letter = 'Lt'
@@ -85,9 +93,6 @@ class Category(StrEnum):
         )
 
 
-# --------------------------------------------------------------------------------------
-
-
 class EastAsianWidth(StrEnum):
     Ambiguous = 'A'
     Fullwidth = 'F'
@@ -110,3 +115,97 @@ class EastAsianWidth(StrEnum):
     @property
     def is_wide(self) -> bool:
         return self in (EastAsianWidth.Fullwidth, EastAsianWidth.Wide)
+
+
+class BinaryProperty(StrEnum):
+    """Supported binary Unicode properties"""
+    Dash = 'Dash'
+    Emoji = 'Emoji'
+    Emoji_Component = 'EComp'
+    Emoji_Modifier = 'EMod'
+    Emoji_Modifier_Base = 'EBase'
+    Emoji_Presentation = 'EPres'
+    Extended_Pictographic = 'ExtPict'
+    Noncharacter_Code_Point = 'NChar'
+    Variation_Selector = 'VS'
+    White_Space = 'WSpace'
+
+    @property
+    def is_emoji(self) -> bool:
+        """
+        Determine whether the property relates to emoji, i.e., is `Emoji`,
+        is prefixed with `Emoji_`, or is `Extended_Pictographic`.
+        """
+        return self in _EMOJI_PROPERTIES
+
+
+_EMOJI_PROPERTIES = frozenset([
+    BinaryProperty.Emoji,
+    BinaryProperty.Emoji_Component,
+    BinaryProperty.Emoji_Modifier,
+    BinaryProperty.Emoji_Modifier_Base,
+    BinaryProperty.Emoji_Presentation,
+    BinaryProperty.Extended_Pictographic,
+])
+
+
+# --------------------------------------------------------------------------------------
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class CharacterData:
+    """A Unicode code point and its properties."""
+
+    codepoint: CodePoint
+    category: Category
+    east_asian_width: EastAsianWidth
+    age: None | str
+    name: None | str
+    block: None | str
+    flags: frozenset[BinaryProperty]
+
+    @property
+    def is_zero_width(self) -> bool:
+        return (
+            self.category in (
+                Category.Enclosing_Mark,
+                Category.Nonspacing_Mark,
+                Category.Format
+            ) and self.codepoint != 0x00AD
+            or 0x1160 <= self.codepoint <= 0x11FF
+            or self.codepoint == 0x200B
+        )
+
+    def wcwidth(self) -> int:
+        # https://www.cl.cam.ac.uk/~mgk25/ucs/wcwidth.c
+        # https://github.com/jquast/wcwidth
+        if self.codepoint == 0 or self.is_zero_width:
+            return 0
+        if self.codepoint < 32 or 0x7F <= self.codepoint < 0xA0:
+            return -1
+        if self.east_asian_width.is_wide:
+            return 2
+
+        return 1
+
+    def __str__(self) -> str:
+        return f'{self.codepoint!s:<8} {format_properties(self)}'
+
+
+def format_properties(
+    data: CharacterData,
+    *,
+    flag_width: int = 25,
+    max_width: None | int = None
+) -> str:
+    width = data.east_asian_width
+    flags = ' '.join(f.value for f in data.flags)
+    age = data.age or ''
+    name = data.name or ''
+    block = '' if data.block is None else f' ({data.block})'
+    props = (
+        f'{data.category.value} {width:<2} {flags:<{flag_width}} {age:>4} {name}{block}'
+    )
+    if max_width is not None and len(props) > max_width:
+        props = props[:max_width - 1] + '…'
+    return props

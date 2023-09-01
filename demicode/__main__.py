6f276c64bcbcd3b54e38a0e83847830700010af4
Robert Grimm <apparebit@gmail.com>
2023-09-01 15:59:55 -0400

    edit for error message language

--- demicode/__main__.py
+++ demicode/__main__.py
@@ -4,8 +4,8 @@ def main() -> None:
     if sys.version_info < (3, 11, 0):
         version = '.'.join(str(v) for v in sys.version_info[:3])
         print(f'Error: You are trying to run demicode with Python {version},')
-        print(f'but demicode requires at least Python 3.11.0 or later')
-        print(f'Please try again after upgrading your Python installation.')
+        print(f'but demicode requires Python 3.11.0 or later to run.')
+        print(f"Please upgrade the Python version and then try again.")
         sys.exit(1)
 
     # Delay importing tool, so that the version check above always proceeds.

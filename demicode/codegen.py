49758b2889134d93e8a94bd7daa243e469be9727
Robert Grimm <apparebit@gmail.com>
2023-09-06 21:41:22 -0400

    use appropriate Unicode versions for code generation, document why

--- demicode/codegen.py
+++ demicode/codegen.py
@@ -3,20 +3,34 @@ from collections import defaultdict
 from pathlib import Path
 import re
 
-from .mirror import mirror_unicode_data
+from .mirror import mirror_unicode_data, retrieve_latest_ucd_version
 from .model import ComplexProperty, Version
 from .parser import ingest
 
 
-def generate_code(root: Path, version: Version) -> None:
-    property_values = retrieve_property_values(root, version)
+def generate_code(root: Path) -> None:
+    # We assume that obsolete property values won't be deleted (which is the
+    # case for grapheme cluster breaking). Hence the most recent version of
+    # Unicode is just the right version for enumerations of property values.
+    property_values = retrieve_property_values(root, retrieve_latest_ucd_version(root))
     with open('demicode/_property.py', mode='w', encoding='utf8') as file:
         for line in generate_property_values(property_values):
             print(line, file=file)
 
+    # Since algorithms may and do change, testing the results of a particular
+    # algorithm is version-specific. Hence specifically targeted versions are
+    # just the right versions for testing.
+    v150 = Version(15, 0, 0)
+    path150 = mirror_unicode_data(root, 'GraphemeBreakTest.txt', v150)
+
     with open('test/grapheme_clusters.py', mode='w', encoding='utf8') as file:
-        for line in generate_grapheme_clusters(root, version):
+        print('# This module is machine-generated. Do not edit by hand.\n', file=file)
+        for line in generate_grapheme_cluster_breaks(path150, v150):
             print(line, file=file)
+
+
+# --------------------------------------------------------------------------------------
+# Property Values
 
 
 def retrieve_property_values(
@@ -64,17 +78,16 @@ def generate_property_values(
             yield ''
 
 
+# --------------------------------------------------------------------------------------
+# Grapheme Cluster Breaks
+
+
 MARK = re.compile(r'[÷×]')
 
-def generate_grapheme_clusters(root: Path, version: Version) -> Iterator[str]:
+def generate_grapheme_cluster_breaks(path: Path, version: Version) -> Iterator[str]:
     # Convert into dictionary entries, ready for testing.
     # https://www.unicode.org/Public/UCD/latest/ucd/auxiliary/GraphemeBreakTest.txt
-    path = mirror_unicode_data(root, 'GraphemeBreakTest.txt', version)
-
-    yield '# This module is machine-generated. Do not edit by hand.'
-    yield ''
-    yield ''
-    yield 'GRAPHEME_CLUSTERS = {'
+    yield f'GRAPHEME_CLUSTER_BREAKS_{version.major}_{version.minor} = {{'
 
     with open(path, mode='r', encoding='utf8') as file:
         for line in file:
@@ -90,5 +103,3 @@ def generate_grapheme_clusters(root: Path, version: Version) -> Iterator[str]:
             yield f'    ({codepoints}): ({marks}),'
 
     yield '}'
-    yield ''
-    yield ''

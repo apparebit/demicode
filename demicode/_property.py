8015658a6096633d332c5b7a6d96f3238485e32f
Robert Grimm <apparebit@gmail.com>
2023-09-09 21:24:35 -0400

    capture snapshot after codegen changes

--- demicode/_property.py
+++ demicode/_property.py
@@ -1,6 +1,6 @@
 # This module is machine-generated. Do not edit by hand.
 
-from enum import StrEnum
+from enum import IntEnum, StrEnum
 
 
 __all__ = (
@@ -12,65 +12,65 @@ __all__ = (
 )
 
 
-class CanonicalCombiningClass(StrEnum):
-    NR = "0"
-    OV = "1"
-    HANR = "6"
-    NK = "7"
-    KV = "8"
-    VR = "9"
-    CCC10 = "10"
-    CCC11 = "11"
-    CCC12 = "12"
-    CCC13 = "13"
-    CCC14 = "14"
-    CCC15 = "15"
-    CCC16 = "16"
-    CCC17 = "17"
-    CCC18 = "18"
-    CCC19 = "19"
-    CCC20 = "20"
-    CCC21 = "21"
-    CCC22 = "22"
-    CCC23 = "23"
-    CCC24 = "24"
-    CCC25 = "25"
-    CCC26 = "26"
-    CCC27 = "27"
-    CCC28 = "28"
-    CCC29 = "29"
-    CCC30 = "30"
-    CCC31 = "31"
-    CCC32 = "32"
-    CCC33 = "33"
-    CCC34 = "34"
-    CCC35 = "35"
-    CCC36 = "36"
-    CCC84 = "84"
-    CCC91 = "91"
-    CCC103 = "103"
-    CCC107 = "107"
-    CCC118 = "118"
-    CCC122 = "122"
-    CCC129 = "129"
-    CCC130 = "130"
-    CCC132 = "132"
-    CCC133 = "133"
-    ATBL = "200"
-    ATB = "202"
-    ATA = "214"
-    ATAR = "216"
-    BL = "218"
-    B = "220"
-    BR = "222"
-    L = "224"
-    R = "226"
-    AL = "228"
-    A = "230"
-    AR = "232"
-    DB = "233"
-    DA = "234"
-    IS = "240"
+class CanonicalCombiningClass(IntEnum):
+    NR = 0
+    OV = 1
+    HANR = 6
+    NK = 7
+    KV = 8
+    VR = 9
+    CCC10 = 10
+    CCC11 = 11
+    CCC12 = 12
+    CCC13 = 13
+    CCC14 = 14
+    CCC15 = 15
+    CCC16 = 16
+    CCC17 = 17
+    CCC18 = 18
+    CCC19 = 19
+    CCC20 = 20
+    CCC21 = 21
+    CCC22 = 22
+    CCC23 = 23
+    CCC24 = 24
+    CCC25 = 25
+    CCC26 = 26
+    CCC27 = 27
+    CCC28 = 28
+    CCC29 = 29
+    CCC30 = 30
+    CCC31 = 31
+    CCC32 = 32
+    CCC33 = 33
+    CCC34 = 34
+    CCC35 = 35
+    CCC36 = 36
+    CCC84 = 84
+    CCC91 = 91
+    CCC103 = 103
+    CCC107 = 107
+    CCC118 = 118
+    CCC122 = 122
+    CCC129 = 129
+    CCC130 = 130
+    CCC132 = 132
+    CCC133 = 133
+    ATBL = 200
+    ATB = 202
+    ATA = 214
+    ATAR = 216
+    BL = 218
+    B = 220
+    BR = 222
+    L = 224
+    R = 226
+    AL = 228
+    A = 230
+    AR = 232
+    DB = 233
+    DA = 234
+    IS = 240
 
 
 class EastAsianWidth(StrEnum):
@@ -138,6 +138,7 @@ class IndicSyllabicCategory(StrEnum):
     Consonant_Placeholder = "Consonant_Placeholder"
     Consonant_Preceding_Repha = "Consonant_Preceding_Repha"
     Consonant_Prefixed = "Consonant_Prefixed"
+    Consonant_Repha = "Consonant_Repha"
     Consonant_Subjoined = "Consonant_Subjoined"
     Consonant_Succeeding_Repha = "Consonant_Succeeding_Repha"
     Consonant_With_Stacker = "Consonant_With_Stacker"
@@ -328,5 +329,3 @@ class Script(StrEnum):
     Inherited = "Zinh"
     Common = "Zyyy"
     Unknown = "Zzzz"
-
-

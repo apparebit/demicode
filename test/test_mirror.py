466d483f88a5f59fc9a0178f01f363b1e6efb3dc
Robert Grimm <apparebit@gmail.com>
2023-09-09 21:26:29 -0400

    test URL creation for versions that do and do not result in versioning errors

---
+++ test/test_mirror.py
@@ -0,0 +1,58 @@
+import unittest
+
+from demicode.mirror import ucd_url_of
+from demicode.model import Version, VersioningError
+
+class TestMirror(unittest.TestCase):
+
+    def test_versioning_errors(self) -> None:
+        with self.assertRaises(VersioningError):
+            ucd_url_of('IndicSyllabicCategory.txt', Version(5, 0, 0))
+
+        with self.assertRaises(VersioningError):
+            ucd_url_of('emoji-variation-sequences.txt', Version(9, 0, 0))
+
+        with self.assertRaises(VersioningError):
+            ucd_url_of('emoji-test.txt', Version(9, 0, 0))
+
+        with self.assertRaises(VersioningError):
+            ucd_url_of('emoji-sequences.txt', Version(8, 0, 0))
+
+        with self.assertRaises(VersioningError):
+            ucd_url_of('emoji-data.txt', Version(7, 0, 0))
+
+    def test_urls(self) -> None:
+        self.assertEqual(
+            ucd_url_of('ReadMe.txt', Version(6, 0, 0)),
+            'https://www.unicode.org/Public/UCD/latest/ReadMe.txt'
+        )
+
+        self.assertEqual(
+            ucd_url_of('IndicSyllabicCategory.txt', Version(6, 0, 0)),
+            'https://www.unicode.org/Public/6.0.0/ucd/IndicSyllabicCategory.txt'
+        )
+
+        self.assertEqual(
+            ucd_url_of('emoji-variation-sequences.txt', Version(10, 0, 0)),
+            'https://www.unicode.org/Public/emoji/5.0/emoji-variation-sequences.txt'
+        )
+
+        self.assertEqual(
+            ucd_url_of('emoji-test.txt', Version(10, 0, 0)),
+            'https://www.unicode.org/Public/emoji/5.0/emoji-test.txt'
+        )
+
+        self.assertEqual(
+            ucd_url_of('emoji-sequences.txt', Version(9, 0, 0)),
+            'https://www.unicode.org/Public/emoji/3.0/emoji-sequences.txt'
+        )
+
+        self.assertEqual(
+            ucd_url_of('emoji-data.txt', Version(8, 0, 0)),
+            'https://www.unicode.org/Public/emoji/1.0/emoji-data.txt'
+        )
+
+        self.assertEqual(
+            ucd_url_of('emoji-data.txt', Version(13, 0, 0)),
+            'https://www.unicode.org/Public/13.0.0/ucd/emoji/emoji-data.txt'
+        )

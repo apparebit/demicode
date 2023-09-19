3cc379418c50747d5e3ab0f0098474fcf1dca896
Robert Grimm <apparebit@gmail.com>
2023-09-19 00:04:41 -0400

    show bit sizes too, add another, smaller tabulation of properties

--- demicode/statistics.py
+++ demicode/statistics.py
@@ -1,7 +1,8 @@
-from collections import defaultdict
+import math
 from pathlib import Path
-from typing import cast
+from typing import cast, NamedTuple
 
+import demicode.model as model
 from .model import BinaryProperty, Property, Version
 from .render import Renderer
 from .ucd import UnicodeCharacterDatabase
@@ -19,47 +20,67 @@ _PROPERTIES: tuple[BinaryProperty | Property, ...] = (
     Property.East_Asian_Width,
     Property.General_Category,
     Property.Grapheme_Cluster_Break,
+    Property.Indic_Conjunct_Break,
     Property.Indic_Syllabic_Category,
     Property.Script,
 )
 
 
+class PropertyInfo(NamedTuple):
+    bits: int        # The number of bits to capture all property values
+    points: int      # The number of code points with non-default values
+    ranges: int      # The number of code point ranges used in UCD file
+    max_ranges: int  # The number of code point ranges after combining adjacent ones
+
+
 def collect_statistics(
     root: Path, version: Version
-) -> dict[BinaryProperty | Property, list[int]]:
+) -> dict[BinaryProperty | Property, PropertyInfo]:
     """
-    For the UCD version cached at the given path, before and after optimizing
-    its internal representation, and for each of several Unicode properties,
-    count the number of code points with property values other than the default
-    and the number of code point ranges used to represent that information. If a
-    property's number of code points before and after optimizing diverges, this
-    function raises an exception.
+    For the UCD version cached at the given path and for each of several Unicode
+    properties, collect the number of distinct values for the Unicode property,
+    the number of code points with values other than the default, the number of
+    code point ranges in the UCD file, and the number of code point ranges after
+    combining adjacent ranges for the same property value.
     """
-    data: defaultdict[BinaryProperty | Property, list[int]] = defaultdict(list)
-
     ucd = UnicodeCharacterDatabase(root, version).prepare().validate()
     if ucd.is_optimized:
         raise AssertionError('UCD claims to be optimized without call to optimize()')
+    counts: list[tuple[int, int]] = []
     for property in _PROPERTIES:
-        data[property].extend(ucd.count_values(property))
+        counts.append(ucd.count_values(property))
 
     ucd.optimize().validate()
     if not getattr(ucd, 'is_optimized'):  # Work around mypy bug
         raise AssertionError('UCD claims not to be optimized after call to optimize()')
-    for property in _PROPERTIES:
-        data[property].extend(ucd.count_values(property))
 
-    for property in _PROPERTIES:
-        counts = data[property]
-        assert counts[0] == counts[2],\
-            f'{property} should have {counts[0]} == {counts[2]} code points'
+    stats: dict[BinaryProperty | Property, PropertyInfo] = {}
+    for property, (points, ranges) in zip(_PROPERTIES, counts):
+        if isinstance(property, BinaryProperty):
+            bits = 1
+        elif property is Property.Canonical_Combining_Class:
+            # The enumeration defines constants only for some values, but the
+            # values are drawn from 0-240. Hence we need 8 bits.
+            bits = 8
+        else:
+            values = getattr(model, property.name.replace('_', ''))
+            bits = math.ceil(math.log2(len(values)))
 
-    return data
+        points2, max_ranges = ucd.count_values(property)
+        if points != points2:
+            raise AssertionError(
+                f'Property {property.name} has {points} != {points2} '
+                'non-default codepoints'
+            )
+
+        stats[property] = PropertyInfo(bits, points, ranges, max_ranges)
+
+    return stats
 
 
 def show_statistics(
     version: Version,
-    data: dict[BinaryProperty | Property, list[int]],
+    data: dict[BinaryProperty | Property, PropertyInfo],
     renderer: Renderer,
 ) -> None:
     print()
@@ -68,25 +89,32 @@ def show_statistics(
     print()
 
     def show_heading(text: str) -> None:
-        print(renderer.hint(f' {text:<25}     Points  Ranges  MinRng'))
+        print(renderer.hint(f' {text:<25}  Bt     Points  Ranges  MinRng'))
         print()
 
-    sum_points = sum_ranges = sum_min_ranges = 0
+    sum_bits = sum_points = sum_ranges = sum_max_ranges = 0
 
     def show_counts(property: BinaryProperty | Property) -> None:
-        nonlocal sum_points, sum_ranges, sum_min_ranges
+        nonlocal sum_bits, sum_points, sum_ranges, sum_max_ranges
 
-        points, ranges, _, min_ranges = data[property]
+        bits, points, ranges, max_ranges = data[property]
+        sum_bits += bits
         sum_points += points
         sum_ranges += ranges
-        sum_min_ranges += min_ranges
+        sum_max_ranges += max_ranges
 
-        print(f' {property.name:<25}  {points:9,d}  {ranges:6,d}  {min_ranges:6,d}')
+        print(
+            f' {property.name:<25}  {bits:2,d}  {points:9,d}  '
+            f'{ranges:6,d}  {max_ranges:6,d}'
+        )
 
     def show_total() -> None:
-        print(f' {" " * 25}  {renderer.hint("–" * (9 + 2 + 6 + 2 + 6))}' )
+        print(f' {" " * 25}  {renderer.hint("–" * (2 + 2 + 9 + 2 + 6 + 2 + 6))}' )
         heading = renderer.hint(f'{"Subtotal":<25}')
-        print(f' {heading}  {sum_points:9,d}  {sum_ranges:6,d}  {sum_min_ranges:6,d}')
+        print(
+            f' {heading}  {sum_bits:2,d}  {sum_points:9,d}  '
+            f'{sum_ranges:6,d}  {sum_max_ranges:6,d}'
+        )
         print('\n')
 
     property: BinaryProperty | Property
@@ -104,12 +132,13 @@ def show_statistics(
     show_total()
 
     show_heading('Complex Properties')
-    sum_points = sum_ranges = sum_min_ranges = 0
+    sum_bits = sum_points = sum_ranges = sum_max_ranges = 0
     for property in (
         Property.Canonical_Combining_Class,
         Property.East_Asian_Width,
         Property.General_Category,
         Property.Grapheme_Cluster_Break,
+        Property.Indic_Conjunct_Break,
         Property.Indic_Syllabic_Category,
         Property.Script,
     ):
@@ -120,8 +149,8 @@ def show_statistics(
     show_counts(Property.Emoji_Sequence)
     print('\n')
 
-    show_heading('Required Properties')
-    sum_points = sum_ranges = sum_min_ranges = 0
+    show_heading('Required Properties I')
+    sum_bits = sum_points = sum_ranges = sum_max_ranges = 0
     for property in cast(tuple[BinaryProperty | Property, ...], (
         BinaryProperty.Emoji_Presentation,
         BinaryProperty.Extended_Pictographic,
@@ -134,3 +163,21 @@ def show_statistics(
     )):
         show_counts(property)
     show_total()
+
+    show_heading('Required Properties II')
+    sum_bits = sum_points = sum_ranges = sum_max_ranges = 0
+    for property in cast(tuple[BinaryProperty | Property, ...], (
+        BinaryProperty.Emoji_Presentation,
+        BinaryProperty.Extended_Pictographic,
+        Property.East_Asian_Width,
+        Property.General_Category,
+        Property.Grapheme_Cluster_Break,
+        Property.Indic_Conjunct_Break,
+    )):
+        show_counts(property)
+    show_total()
+
+    print(renderer.hint(
+        'https://github.com/apparebit/demicode/blob/boss/docs/props.md'
+    ))
+    print()

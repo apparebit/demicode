27c699ef6171570a843ee0bdb9d616b17b6cfadc
Robert Grimm <apparebit@gmail.com>
2023-09-26 03:04:51 -0400

    make helpers private, add key press reader

--- demicode/render.py
+++ demicode/render.py
@@ -13,39 +13,43 @@ just before emitting a page. In other words, size updates require polling with
 the `Renderer.refresh()` method.
 """
 
+from collections.abc import Iterator
+from contextlib import contextmanager
 from dataclasses import dataclass
 from enum import StrEnum
 import os
 import sys
-from typing import TextIO
+from typing import Never, TextIO
 
 
 from .codepoint import CodePoint
 
 
-CSI = '\x1b['
+_CSI = '\x1b['
+_OSC = '\x1b]'
+_ST = '\x1b\\'
 
 
-def SGR(code: str) -> str:
+def _SGR(code: str) -> str:
     """Format Select Graphic Rendition."""
-    return f'{CSI}{code}m'
+    return f'{_CSI}{code}m'
 
 
-def CHA(column: int | str) -> str:
+def _CHA(column: int | str) -> str:
     """Format Cursor Horizontal Absolute (an editor function)."""
-    return f'{CSI}{column}G'
+    return f'{_CSI}{column}G'
 
 
-def HPA(column: int | str) -> str:
+def _HPA(column: int | str) -> str:
     """Format Horizontal Position Absolute (a format effector)."""
-    return f'{CSI}{column}`'
+    return f'{_CSI}{column}`'
 
 
-def bg(color: int | str) -> str:
+def _bg(color: int | str) -> str:
     return f'48;5;{color}'
 
 
-def fg(color: int | str) -> str:
+def _fg(color: int | str) -> str:
     return f'38;5;{color}'
 
 
@@ -56,7 +60,6 @@ class Theme:
     blot_highlight: str
     blot_obstruction: str
     hint: str
-    very_strong: str
     error: str
 
     @classmethod
@@ -67,35 +70,33 @@ class Theme:
         blot_highlight: str,
         blot_obstruction: str,
         hint: str,
-        very_strong: str,
         error: str,
     ) -> 'Theme':
         return cls(
-            SGR(legend),
-            SGR(heading),
-            SGR(blot_highlight),
-            SGR(blot_obstruction),
-            SGR(hint),
-            SGR(very_strong),
-            SGR(error),
+            _SGR(legend),
+            _SGR(heading),
+            _SGR(blot_highlight),
+            _SGR(blot_obstruction),
+            _SGR(hint),
+            _SGR(error),
         )
 
 
-class Styles:
-    RESET = SGR('0')
-    BOLD = SGR('1')
-    ITALIC = SGR('3')
+class Style:
+    RESET = _SGR('0')
+    BOLD = _SGR('1')
+    ITALIC = _SGR('3')
 
     LIGHT = (
-        Theme.of(bg(252), fg('246;3'), bg(254), fg(244), fg(246), '1', fg('160;1')),
-        Theme.of(bg(252), fg('246;3'), bg(220), fg(244), fg(246), '1', fg('160;1')),
-        Theme.of(bg(252), fg('246;3'), bg(220), fg(202), fg(246), bg('218;1'), fg('160;1')),
+        Theme.of(_bg(252), _fg('246;3'), _bg(254), _fg(244), _fg(246), _fg('160;1')),
+        Theme.of(_bg(252), _fg('246;3'), _bg(220), _fg(244), _fg(246), _fg('160;1')),
+        Theme.of(_bg(252), _fg('246;3'), _bg(220), _fg(202), _fg(246), _fg('160;1')),
     )
 
     DARK = (
-        Theme.of(bg(240), fg('245;3'), bg(238), fg(243), fg(245), '1', fg('88;1')),
-        Theme.of(bg(240), fg('245;3'), bg(53),  fg(243), fg(245), '1', fg('88;1')),
-        Theme.of(bg(240), fg('245;3'), bg(53),  fg(93),  fg(245), bg('218;1'), fg('88;1')),
+        Theme.of(_bg(240), _fg('245;3'), _bg(238), _fg(243), _fg(245), _fg('88;1')),
+        Theme.of(_bg(240), _fg('245;3'), _bg(53),  _fg(243), _fg(245), _fg('88;1')),
+        Theme.of(_bg(240), _fg('245;3'), _bg(53),  _fg(93),  _fg(245), _fg('88;1')),
     )
 
     @classmethod
@@ -105,6 +106,11 @@ class Styles:
     @classmethod
     def italic(cls, text: str) -> str:
         return f'{cls.ITALIC}{text}{cls.RESET}'
+
+    @classmethod
+    def link(cls, href: str, text: None | str = None) -> str:
+        text = href if text is None else text
+        return f'{_OSC}8;;{href}{_ST}{text}{_OSC}8;;{_ST}'
 
 
 class Mode(StrEnum):
@@ -117,15 +123,104 @@ class Padding(StrEnum):
     FOREGROUND = str(CodePoint.FULL_BLOCK)
 
 
+if sys.platform in ('linux', 'darwin'):
+
+    import termios
+    import tty
+
+    class MalformedEscape(Exception):
+        pass
+
+    class KeyPressReader(Iterator[bytes]):
+        """
+        A class to read key presses without waiting for a newline. The file
+        descriptor must be associated with a TTY in cbreak mode.
+        """
+
+        __slots__ = ('_fileno',)
+
+        def __init__(self, fileno: int) -> None:
+            self._fileno = fileno
+
+        def __next__(self) -> bytes:
+            return self.read()
+
+        def read(self) -> bytes:
+            """
+            Read the next pressed key or key combination. If the key or key
+            combination corresponds to an ASCII character, this method returns
+            a one-byte sequence with that character. For all other keys and
+            key combinations, it returns a multibyte sequence.
+            """
+            return os.read(self._fileno, 3)
+
+        def read_escape(self) -> bytes:
+            """
+            Read a complete escape sequence from input. This method recognizes
+            the different escape sequence patterns and accumulates all bytes
+            belonging to such a sequence. It raises an exception on malformed
+            escape sequences.
+            """
+            buffer = bytearray()
+
+            def next_byte() -> int:
+                b = os.read(self._fileno, 1)[0]
+                buffer.append(b)
+                return b
+
+            def bad_byte(b: int) -> Never:
+                raise MalformedEscape(f'unexpected key code 0x{b:02X}')
+
+            b = next_byte()
+            if b != 0x1B:
+                bad_byte(b)
+
+            b = next_byte()
+            if b == 0x5B:  # [
+                # CSI has structure
+                b = next_byte()
+                while 0x30 <= b <= 0x3F:
+                    b = next_byte()
+                while 0x20 <= b <= 0x2F:
+                    b = next_byte()
+                if 0x40 <= b <= 0x7E:
+                    return bytes(buffer)
+                bad_byte(b)
+
+            if b in (0x50, 0x58, 0x5D, 0x5E, 0x5F):  # P,X,],^,_
+                # DCS, SOS, OSC, PM, and APC end with ST only
+                b = next_byte()
+                while b not in (0x07, 0x1B):
+                    b = next_byte()
+                if b == 0x07:
+                    return bytes(buffer)
+                b = next_byte()
+                if b == 0x5C:  # \\
+                    return bytes(buffer)
+                bad_byte(b)
+
+            while 0x20 <= b <= 0x2F:
+                b = next_byte()
+            if 0x30 <= b <= 0x7E:
+                return bytes(buffer)
+            bad_byte(b)
+
+
 class Renderer:
 
     MAX_WIDTH = 140
 
-    def __init__(self, out: TextIO, mode: Mode, intensity: int) -> None:
-        self._out = out
-        self._interactive = sys.__stdin__.isatty() and out.isatty()
-        self._theme = getattr(Styles, mode.value)[min(2, max(0, intensity))]
+    def __init__(
+        self, input: TextIO, output: TextIO, mode: Mode, intensity: int
+    ) -> None:
+        self._input = input
+        self._output = output
+        self._interactive = input.isatty() and output.isatty()
+        self._theme = getattr(Style, mode.value)[min(2, max(0, intensity))]
         self.refresh()
+
+    # ----------------------------------------------------------------------------------
+    # Read Terminal Properties
 
     @property
     def is_interactive(self) -> bool:
@@ -156,8 +251,26 @@ class Renderer:
     def width(self) -> int:
         return self._width
 
+    # ----------------------------------------------------------------------------------
+    # Read from Terminal
+
+    has_reader = False
+
+    @contextmanager
+    def reader(self) -> Iterator[KeyPressReader]:
+        raise NotImplementedError('Renderer.reader()')
+
+    def get_cursor(self) -> None | tuple[int, int]:
+        return None
+
+    # ----------------------------------------------------------------------------------
+    # Update Terminal
+
     def set_window_title(self, text: str) -> None:
         pass
+
+    # ----------------------------------------------------------------------------------
+    # Format Text
 
     def fit(self, text: str, *, width: None | int = None, fill: bool = False) -> str:
         if width is None:
@@ -166,7 +279,7 @@ class Renderer:
             return text.ljust(width) if fill else text
         return text[:width - 1] + '…'
 
-    def column(self, column: int) -> str:
+    def adjust_column(self, column: int) -> str:
         return ''
 
     def legend(self, text: str) -> str:
@@ -190,20 +303,24 @@ class Renderer:
     def strong(self, text: str) -> str:
         return text
 
-    def very_strong(self, text: str) -> str:
-        return text
+    def link(self, href: str, text: None | str = None) -> str:
+        return href if text is None else text
 
     def error(self, text: str) -> str:
         return text
 
-    def print(self, text: str) -> None:
-        self._out.write(text)
-        self._out.flush()
+    # ----------------------------------------------------------------------------------
+    # Print Formatted Text
 
-    def println(self, text: str) -> None:
-        self._out.write(text)
-        self._out.write('\n')
-        self._out.flush()
+    def print(self, text: str) -> None:
+        self._output.write(text)
+        self._output.flush()
+
+    def println(self, text: str = '') -> None:
+        if text:
+            self._output.write(text)
+        self._output.write('\n')
+        self._output.flush()
 
 
 class StyledRenderer(Renderer):
@@ -213,18 +330,40 @@ class StyledRenderer(Renderer):
     def has_style(self) -> bool:
         return True
 
+    if sys.platform in ('linux', 'darwin'):
+        has_reader = True
+
+        @contextmanager
+        def reader(self) -> Iterator[KeyPressReader]:
+            fileno = self._input.fileno()
+            settings = termios.tcgetattr(fileno)
+            tty.setcbreak(fileno)
+            try:
+                yield KeyPressReader(fileno)
+            finally:
+                termios.tcsetattr(fileno, termios.TCSADRAIN, settings)
+
+        def get_cursor(self) -> None | tuple[int, int]:
+            with self.reader() as reader:
+                self.print(f'{_CSI}6n')
+                response = reader.read_escape()
+            if not response.startswith(b'\x1B[') or not response.endswith(b'R'):
+                return None
+            w, _, h = response[2:-1].partition(b';')
+            return int(w), int(h)
+
     def set_window_title(self, text: str) -> None:
         if self.is_interactive:
-            self.print(f'\x1b]2;{text}\x07')
+            self.print(f'{_OSC}0;{text}{_ST}')
 
-    def column(self, column: int) -> str:
-        return CHA(column)
+    def adjust_column(self, column: int) -> str:
+        return _CHA(column)
 
     def legend(self, text: str) -> str:
-        return f'{self._theme.legend}{text}{Styles.RESET}'
+        return f'{self._theme.legend}{text}{Style.RESET}'
 
     def heading(self, text: str) -> str:
-        return f'{self._theme.heading}{text}{Styles.RESET}'
+        return f'{self._theme.heading}{text}{Style.RESET}'
 
     def blot(self, text: str, padding: Padding, width: int) -> str:
         if padding is Padding.BACKGROUND:
@@ -232,27 +371,27 @@ class StyledRenderer(Renderer):
                 text
                 + self._theme.blot_highlight
                 + (padding.value * width)
-                + Styles.RESET
+                + Style.RESET
             )
         else:
             return (
                 text
                 + self._theme.blot_obstruction
                 + (padding.value * width)
-                + Styles.RESET
+                + Style.RESET
             )
 
     def hint(self, text:str) -> str:
-        return f'{self._theme.hint}{text}{Styles.RESET}'
+        return f'{self._theme.hint}{text}{Style.RESET}'
 
     def em(self, text: str) -> str:
-        return f'{Styles.ITALIC}{text}{Styles.RESET}'
+        return Style.italic(text)
 
     def strong(self, text: str) -> str:
-        return f'{Styles.BOLD}{text}{Styles.RESET}'
+        return Style.bold(text)
 
-    def very_strong(self, text: str) -> str:
-        return f'{self._theme.very_strong}{text}{Styles.RESET}'
+    def link(self, href: str, text: None | str = None) -> str:
+        return Style.link(href, text)
 
     def error(self, text: str) -> str:
-        return f'{self._theme.error}{text}{Styles.RESET}'
+        return f'{self._theme.error}{text}{Style.RESET}'

cc0126a13bc882039139e57882bcae3174960bb0
Robert Grimm <apparebit@gmail.com>
2023-11-05 23:11:17 -0500

    support more than one payload out of three (for now)

--- script/badterm.py
+++ script/badterm.py
@@ -6,7 +6,7 @@ import sys
 
 sys.path.insert(0, '')
 
-from demicode.ui.terminal import report_terminal_version
+from demicode.ui.terminal import determine_terminal_and_version
 
 
 CSI = '\x1b['
@@ -21,19 +21,13 @@ PREFIX = '\u2E3B\u2A0C{sep}\U0001F9D1\u200D\U0001F4BB'
 WIDTH = 29
 
 
-def parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser('badterm', 'usage: badterm [-x]')
-    parser.add_argument('-x', action='store_true', dest='extra')
-    return parser
-
-
 def mkbar() -> str:
     width, _ = os.get_terminal_size()
     return '\x1b[48;5;196m' + (' ' * width) + '\x1b[0m'
 
 
 def mklabels() -> tuple[str, str]:
-    terminal, version = report_terminal_version()
+    terminal, version = determine_terminal_and_version()
     if terminal is None:
         return 'Unknown Terminal', ''
     elif version is None:
@@ -48,7 +42,7 @@ def mkprefix(spaces: int) -> str:
     return f'\u2E3B\u2A0C{" " * spaces}\U0001F9D1\u200D\U0001F4BB'
 
 
-def print_test(bar1: str, label1: str, label2: str, payload: str, bar2: str) -> None:
+def print_payload(bar1: str, label1: str, label2: str, payload: str, bar2: str) -> None:
     print(bar1)
     print('\n')
     print(f'{INDENT}{LEGEND}{label1.center(WIDTH)}{RESET}')
@@ -64,21 +58,30 @@ def print_test(bar1: str, label1: str, label2: str, payload: str, bar2: str) -> 
 
 
 def main() -> None:
-    options = parser().parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'payload',
+        choices=['dash-integral', 'spaced-dash-integral', 'arab-ligature'],
+    )
+    options = parser.parse_args()
 
     bar = mkbar()
     label1, label2 = mklabels()
 
-    if options.extra:
-        payload = ''.join(mkprefix(w) for w in range(3))
-        print_test(bar, label1, label2, payload, bar)
-        return
+    payload2 = None
+    if options.payload == 'arab-ligature':
+        payload1 = '\uFDFD'
+    elif options.payload == 'spaced-dash-integral':
+        payload1 = ' {FAINT}█{RESET} '.join(mkprefix(w) for w in range(3))
+    elif options.payload == 'dash-integral':
+        payload1 = mkprefix(0) + '\uFF0A𝔽𝕚𝕩𝕖𝕕-𝐖𝐢𝐝𝐭𝐡'
+        payload2 = mkprefix(3) + f'{FAINT}█{RESET}\uFF0A{FAINT}█{RESET}𝔽𝕚𝕩𝕖𝕕-𝐖𝐢𝐝𝐭𝐡'
+    else:
+        raise ValueError(f'invalid payload "{options.payload}"')
 
-    payload1 = mkprefix(0) + '\uFF0A𝔽𝕚𝕩𝕖𝕕-𝐖𝐢𝐝𝐭𝐡'
-    payload2 = mkprefix(3) + f'{FAINT}█{RESET}\uFF0A{FAINT}█{RESET}𝔽𝕚𝕩𝕖𝕕-𝐖𝐢𝐝𝐭𝐡'
-
-    print_test(bar, label1, label2, payload1, '')
-    print_test(bar, label1, label2, payload2, bar)
+    print_payload(bar, label1, label2, payload1, '' if payload2 else bar)
+    if payload2:
+        print_payload(bar, label1, label2, payload2, bar)
 
 if __name__ == '__main__':
     main()

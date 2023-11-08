35cc558dd955d6c3fd418fb29c508c4add21b90f
Robert Grimm <apparebit@gmail.com>
2023-11-08 03:58:11 -0500

    simplify registry initialization and terminmal introspection, remove size checker

--- demicode/ui/terminal.py
+++ demicode/ui/terminal.py
@@ -3,42 +3,14 @@ import dataclasses
 import os
 import subprocess
 import sys
-from typing import ClassVar, Self, TextIO
+from typing import ClassVar, Self
 
 from .render import Renderer
 
 
 @dataclasses.dataclass(frozen=True, slots=True)
 class Terminal:
-    """
-    The many names of a terminal.
 
-    This class captures the `name`, which is the same as the same as the one
-    appearing in the macOS menubar and thus suitable for use in AppleScript when
-    the application is already running.
-
-    Alas, since the application's on-disk name may differ, AppleScript may just
-    require a different name for activation. For example, 'Visual Studio Code'
-    is called just that on disk, but 'Code' in the menu bar. Thankfully,
-    AppleScript can also activate an application based on the `bundle`
-    identifier.
-
-    The nickname is a memorable, lowercase name intended to uniquely identify
-    the application on demicode's command line. It defaults to the terminal's
-    name in lower case, but requires a few explicit overrides.
-
-    For some terminals, a fourth name is registered, but only for looking up the
-    terminal record. It is the value of the TERM_PROGRAM environment variable if
-    different from all other names.
-
-    TODO: This module supports three methods for identifying a terminal at
-    runtime, based on the TERM_PROGRAM environment variable, the bundle
-    identifier available through the __CFBundleIdentifier environment variable,
-    and the XTVERSION control sequence. Unfortunately, only the third method is
-    not specific to macOS. However, support for XTVERSION, at least amongst
-    terminal emulators running on macOS, is sparse. We need more, cross-platform
-    methods.
-    """
     name: str
     bundle: str
     nickname: str
@@ -53,7 +25,6 @@ class Terminal:
     def is_vscode(self) -> bool:
         return self.bundle == 'com.microsoft.VSCode'
 
-
     _registry: ClassVar[None | dict[str, Self]] = None
 
     @classmethod
@@ -61,31 +32,25 @@ class Terminal:
         if cls._registry is None:
             cls._registry = {}
 
-            for names in (
-                # A terminal's name and bundle identifier, optionally followed
-                # by the nickname and TERM_PROGRAM.
-                ('Alacritty', 'org.alacritty'),
-                ('Hyper', 'co.zeit.hyper'),
-                ('iTerm2', 'com.googlecode.iterm2', 'iterm', 'iTerm.app'),
-                ('kitty', 'net.kovidgoyal.kitty'),
-                ('Rio', 'com.raphaelamorim.rio'),
-                ('Terminal', 'com.apple.Terminal', 'terminalapp', 'Apple_Terminal'),
-                ('Code', 'com.microsoft.VSCode', 'vscode'),
-                ('Warp', 'dev.warp.Warp-Stable', 'warp', 'WarpTerminal'),
-                ('WezTerm', 'com.github.wez.wezterm'),
-            ):
+            for names in [
+                # name, bundle identifier, nickname, TERM_PROGRAM
+                ['Alacritty', 'org.alacritty'],
+                ['Hyper', 'co.zeit.hyper'],
+                ['iTerm2', 'com.googlecode.iterm2', 'iterm', 'iTerm.app'],
+                ['kitty', 'net.kovidgoyal.kitty'],
+                ['Rio', 'com.raphaelamorim.rio'],
+                ['Terminal', 'com.apple.Terminal', 'terminalapp', 'Apple_Terminal'],
+                ['Code', 'com.microsoft.VSCode', 'vscode'],
+                ['Warp', 'dev.warp.Warp-Stable', 'warp', 'WarpTerminal'],
+                ['WezTerm', 'com.github.wez.wezterm'],
+            ]:
+                if len(names) == 2:
+                    names.append(names[0].lower())
                 for n in names:
                     assert n not in cls._registry, f'ambiguous terminal identifier {n}'
-                name, bundle, *others = names
 
-                nickname = others[0] if others else name.lower()
-                assert nickname not in cls._registry,\
-                    f'ambiguous terminal identifier {nickname}'
-
-                terminal = cls(name, bundle, nickname)
-                cls._registry[name] = terminal
-                cls._registry[bundle] = terminal
-                for n in others:
+                terminal = cls(*names[:3])
+                for n in names:
                     cls._registry[n] = terminal
 
         return cls._registry
@@ -131,21 +96,13 @@ def inspect_xtversion(
     if not response.startswith(b'\x1BP>|') or not response.endswith(b'\x1B\\'):
         return None, None
 
-    response = response[4:-2]
-    if response[-1] == 41:
-        parts = response.rsplit(b'(', maxsplit=1)
-        if len(parts) != 2:
-            return None, None
-        terminal = parts[0]
-        version = parts[1][:-1]
+    response = response[4:-2].decode('utf8')
+    if response[-1] == ')':
+        terminal, _, version = response.rpartition('(')
+        version = version[1:-1]
     else:
-        parts = response.rsplit(b' ', maxsplit=1)
-        if len(parts) != 2:
-            return None, None
-        terminal, version = parts
-
-    t = terminal.decode('ascii')
-    return Terminal.resolve_name(t), version.decode('ascii')
+        terminal, _, version = response.partition(' ')
+    return Terminal.resolve_name(terminal), version
 
 
 def inspect_bundle_id() -> tuple[None | str, None | str]:
@@ -196,7 +153,7 @@ def determine_terminal_and_version(
     return None, None
 
 
-def join_terminal_version(terminal: None | str, version: None | str) -> str:
+def join_terminal_and_version(terminal: None | str, version: None | str) -> str:
     if terminal is None:
         return 'Unknown Terminal'
     if version is None:
@@ -205,7 +162,7 @@ def join_terminal_version(terminal: None | str, version: None | str) -> str:
 
 
 def termid(renderer: None | Renderer = None) -> str:
-    return join_terminal_version(*determine_terminal_and_version(renderer))
+    return join_terminal_and_version(*determine_terminal_and_version(renderer))
 
 
 if __name__ == '__main__':
@@ -224,40 +181,13 @@ if __name__ == '__main__':
             sys.exit(1)
 
     renderer = Renderer.new()
-    idn = join_terminal_version(*determine_terminal_and_version(renderer))
+    idn = join_terminal_and_version(*determine_terminal_and_version(renderer))
     if show_all:
-        id1 = join_terminal_version(*inspect_env_variables())
-        id2 = join_terminal_version(*inspect_xtversion(renderer))
-        id3 = join_terminal_version(*inspect_bundle_id())
+        id1 = join_terminal_and_version(*inspect_env_variables())
+        id2 = join_terminal_and_version(*inspect_xtversion(renderer))
+        id3 = join_terminal_and_version(*inspect_bundle_id())
         width = max(len(id1), len(id2), len(id3), len(idn)) + 8
         print(f'   env: {id1}\n  ansi: {id2}\nbundle: {id3}\n{"─" * width}')
         print(f' combo: {idn}')
     else:
         print(idn)
-
-
-# --------------------------------------------------------------------------------------
-
-
-class TerminalSizeChecker:
-    """Callable to check that terminal size didn't change on every invocation."""
-
-    def __init__(self, output: None | TextIO = None) -> None:
-        self._fileno = (output or sys.stdout).fileno()
-        self._width, self._height = os.get_terminal_size(self._fileno)
-
-    @property
-    def width(self) -> int:
-        return self._width
-
-    @property
-    def height(self) -> int:
-        return self._height
-
-    def __call__(self) -> None:
-        width, height = os.get_terminal_size(self._fileno)
-        if self._width != width or self._height != height:
-            raise AssertionError(
-                f'terminal size changed from {self._width}×{self._height} '
-                f'to {width}×{height}'
-            )

981d40daee40cefb8bf257594cbccd9ef4400c92
Robert Grimm <apparebit@gmail.com>
2023-11-15 14:27:14 -0500

    roll introspection into Terminal class methods, while making them more robust and composable

--- demicode/ui/terminal.py
+++ demicode/ui/terminal.py
@@ -1,8 +1,11 @@
+import base64
 from collections.abc import Iterator
 import dataclasses
 import os
 import subprocess
+import struct
 import sys
+import time
 from typing import ClassVar, Self
 
 from .termio import TermIO
@@ -10,34 +13,53 @@ from .termio import TermIO
 
 @dataclasses.dataclass(frozen=True, slots=True)
 class Terminal:
-    """
-    The many names of a terminal emulator:
-
-     - `name` is a concise but memorable identifier;
-     - `display` is the less concise version, at least for `VSCode`;
-     - `nickname` is the slightly more concise, lower case version;
-     - `bundle` serves as a unique ID on macOS, is reminiscent of DNS;
-     - `TERM_PROGRAM` may use an altogether different identifier, only used for
-       looking up the terminal's record;
-
-    On macOS, the bundle ID is available through the terminal's
-    __CFBundleIdentifier environment variable. Demicode relies on it for
-    uniquely identifying terminal applications, even when not running on macOS.
-    """
+    """A terminal emulator."""
 
     name: str
     bundle: str
-    nickname: str
+    version: None | str = None
+
+    @property
+    def long_name(self) -> str:
+        if self.is_vscode():
+            return 'Visual Studio Code'
+        return self.name
 
     @property
     def display(self) -> str:
-        return 'Visual Studio Code' if self.is_vscode() else self.name
+        display = self.long_name
+        if self.version:
+            display = f'{display} ({self.version})'
+        return display
+
+    def is_versioned(self) -> bool:
+        return self.version is not None
+
+    def without_version(self) -> Self:
+        return type(self)(self.name, self.bundle)
+
+    def with_version(self, version: str) -> Self:
+        return type(self)(self.name, self.bundle, version)
 
     def is_iterm(self) -> bool:
         return self.bundle == 'com.googlecode.iterm2'
 
+    def is_unknown(self) -> bool:
+        return self.bundle == 'known.unknown'
+
     def is_vscode(self) -> bool:
         return self.bundle == 'com.microsoft.VSCode'
+
+    def is_warp(self) -> bool:
+        return self.bundle == 'dev.warp.Warp-Stable'
+
+    _unknown: ClassVar[None | Self] = None
+
+    @classmethod
+    def unknown(cls) -> Self:
+        if cls._unknown is None:
+            cls._unknown = cls('--unknown--', 'known.unknown')
+        return cls._unknown
 
     _registry: ClassVar[None | dict[str, Self]] = None
 
@@ -46,26 +68,33 @@ class Terminal:
         if cls._registry is None:
             cls._registry = {}
 
-            for names in [
-                # name, bundle identifier, nickname, TERM_PROGRAM
-                ['Alacritty', 'org.alacritty'],
-                ['Hyper', 'co.zeit.hyper'],
-                ['iTerm2', 'com.googlecode.iterm2', 'iterm', 'iTerm.app'],
-                ['Kitty', 'net.kovidgoyal.kitty'],
-                ['Rio', 'com.raphaelamorim.rio'],
-                ['Terminal', 'com.apple.Terminal', 'terminalapp', 'Apple_Terminal'],
-                ['VSCode', 'com.microsoft.VSCode'],
-                ['Warp', 'dev.warp.Warp-Stable', 'warp', 'WarpTerminal'],
-                ['WezTerm', 'com.github.wez.wezterm'],
-            ]:
-                if len(names) == 2:
-                    names.append(names[0].lower())
-                for n in names:
-                    assert n not in cls._registry, f'ambiguous terminal identifier {n}'
-
-                terminal = cls(*names[:3])
-                for n in names:
-                    cls._registry[n] = terminal
+            for names in (
+                # An application's name, its macOS bundle ID, any other names:
+                #   - Alacritty and WezTerm use a different reverse-DNS name
+                #     for Linux desktops
+                #   - iTerm's latest version is 3.4.22, but somehow the '2' is
+                #     sticky at least in the main menu
+                #   - Terminal.app is just a nickname whereas Apple_Terminal
+                #     appears in the TERM_PROGRAM environment variable
+                #   - Visual Studio Code is VSCode's full name
+                #   - WarpTerminal appears in TERM_PROGRAM
+                ('Alacritty', 'org.alacritty', 'org.alacritty.Alacritty'),
+                ('Hyper', 'co.zeit.hyper'),
+                ('iTerm', 'com.googlecode.iterm2', 'iTerm2'),
+                ('Kitty', 'net.kovidgoyal.kitty'),
+                ('Rio', 'com.raphaelamorim.rio'),
+                ('Terminal', 'com.apple.Terminal', 'Terminal.app', 'Apple_Terminal'),
+                ('VSCode', 'com.microsoft.VSCode', 'Visual Studio Code'),
+                ('Warp', 'dev.warp.Warp-Stable', 'WarpTerminal'),
+                ('WezTerm', 'com.github.wez.wezterm', 'org.wezfurlong.wezterm'),
+            ):
+                keys = [n.casefold() for n in names]
+                for key, name in zip(keys, names):
+                    if key in cls._registry:
+                        raise AssertionError(f'duplicate terminal name {name}')
+                terminal = cls(*names[:2])
+                for key in keys:
+                    cls._registry[key] = terminal
 
         return cls._registry
 
@@ -80,93 +109,132 @@ class Terminal:
 
     @classmethod
     def resolve_name(cls, ident: str) -> str:
-        terminal = cls.registry().get(ident)
-        return terminal.display if terminal else ident
+        terminal = cls.registry().get(ident.casefold())
+        return terminal.long_name if terminal else ident
 
     @classmethod
     def resolve(cls, ident: str) -> Self:
-        return cls.registry()[ident]
+        terminal = cls.registry().get(ident.casefold())
+        return terminal if terminal else cls(ident, ident)
 
 
-# --------------------------------------------------------------------------------------
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
+    @classmethod
+    def from_bundle_id(cls) -> None | Self:
+        """Resolve the terminal name based on the macOS bundle identifier"""
+        bundle = os.getenv('__CFBundleIdentifier')
+        return cls.resolve(bundle) if bundle else None
 
-def inspect_xtversion(termio: None | TermIO = None) -> tuple[None | str, None | str]:
-    termio = termio or TermIO()
-    with termio.cbreak_mode():
-        report = termio.request_terminal_version()
-    if report is None:
-        return None, None
-    if report[-1] == ')':
-        terminal, _, version = report.rpartition('(')
-        version = version[1:-1]
-    else:
-        terminal, _, version = report.partition(' ')
-    return Terminal.resolve_name(terminal), version
+    def with_bundle_version(self) -> Self:
+        """Add in the terminal version based on the macOS bundle identifier."""
+        if sys.platform != 'darwin':
+            raise NotImplementedError('only supported on macOS')
 
+        paths = subprocess.run(
+            ["mdfind", f"kMDItemCFBundleIdentifier == '{self.bundle}'"],
+            stdout=subprocess.PIPE,
+            encoding='utf8',
+        ).stdout.splitlines()
+        if len(paths) != 1:
+            return self
 
-def inspect_bundle_id() -> tuple[None | str, None | str]:
-    if not (bundle_id := os.getenv('__CFBundleIdentifier')):
-        return None, None
-    terminal = Terminal.resolve_name(bundle_id)
-
-    paths = subprocess.run(
-        ["mdfind", f"kMDItemCFBundleIdentifier == '{bundle_id}'"],
-        stdout=subprocess.PIPE,
-        encoding='utf8',
-    ).stdout.splitlines()
-
-    if not paths:
-        return terminal, None
-
-    for path in paths:
         version = subprocess.run(
-            ['mdls', '-name', 'kMDItemVersion', '-raw', path],
+            ['mdls', '-name', 'kMDItemVersion', '-raw', paths[0]],
             stdout=subprocess.PIPE,
             encoding='utf8',
         ).stdout
-        if version:
-            return terminal, version
+        return self.with_version(version) if version else self
 
-    return terminal, None
+    @classmethod
+    def from_bundle(cls) -> None | Self:
+        """
+        Resolve the terminal name and version based on the macOS bundle
+        identifier.
+        """
+        t = cls.from_bundle_id()
+        if t is None:
+            return None
+        return t.with_env_version() if t.is_warp() else t.with_bundle_version()
 
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-def inspect_env_variables() -> tuple[None | str, None | str]:
-    if (terminal := os.getenv('TERM_PROGRAM')):
-        terminal = Terminal.resolve_name(terminal)
-    version = os.getenv('TERM_PROGRAM_VERSION')
-    return terminal or None, version or None
+    @classmethod
+    def from_env_name(cls) -> None | Self:
+        """
+        Resolve the terminal name based on the `TERM_PROGRAM` environment
+        variable.
+        """
+        program = os.getenv('TERM_PROGRAM')
+        return cls.resolve(program) if program else None
 
+    def with_env_version(self) -> Self:
+        """
+        Add in the terminal version based on the `TERM_PROGRAM_VERSION`
+        environment variable.
+        """
+        version = os.getenv('TERM_PROGRAM_VERSION')
+        return self.with_version(version) if version else self
 
-def determine_terminal_version(
-    termio: None | TermIO = None
-) -> tuple[None | str, None | str]:
-    termio = termio or TermIO()
-    t, v = inspect_xtversion(termio)
-    if t and v:
-        return t, v
+    @classmethod
+    def from_env(cls) -> None | Self:
+        """
+        Resolve the terminal name and version based on the `TERM_PROGRAM` and
+        `TERM_PROGRAM_VERSION` environment variables.
+        """
+        terminal = cls.from_env_name()
+        return terminal.with_env_version() if terminal else None
 
-    t, v = inspect_bundle_id()
-    if t and v:
-        # The bundle version is horribly outdated.
-        if t == 'Warp':
-            v = os.getenv('TERM_PROGRAM_VERSION')
-        return t, v
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-    t, v = inspect_env_variables()
-    return t, v
+    @classmethod
+    def from_xtversion(cls, termio: None | TermIO = None) -> None | Self:
+        """
+        Resolve the terminal name and version based on the XTVERSION control
+        sequence.
+        """
+        termio = termio or TermIO()
+        try:
+            with termio.cbreak_mode():
+                report = termio.request_terminal_version()
+        except TimeoutError:
+            return None
+        if report is None:
+            return None
+        if report[-1] == ')':
+            name, _, version = report.rpartition('(')
+            version = version[1:-1]
+        else:
+            name, _, version = report.partition(' ')
+        terminal = cls.resolve(name)
+        return terminal.with_version(version) if version else terminal
 
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-def join_terminal_version(terminal: None | str, version: None | str) -> str:
-    if terminal is None:
-        return 'Unknown Terminal'
-    if version is None:
-        return terminal
-    return f'{terminal} ({version})'
+    @classmethod
+    def current(cls, termio: None | TermIO = None) -> Self:
+        """Resolve the terminal name and version."""
+        return (
+            cls.from_xtversion(termio)
+            or cls.from_bundle()
+            or cls.from_env()
+            or cls.unknown()
+        )
 
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-def termid(termio: None | TermIO = None) -> str:
-    return join_terminal_version(*determine_terminal_version(termio))
+    @classmethod
+    def nonce(cls) -> str:
+        value = (
+            base64.b32encode(struct.pack('>I', int(time.time())))
+            .decode('ascii')
+            .lower()
+        )
+        pad = value.index('=')
+        return value[:pad] if pad >= 0 else value
+
+    def filename(self, label: str, nonce: str, suffix: str) -> str:
+        return f'{label}-{nonce}-{self.name.lower()}{suffix}'
 
 
 if __name__ == '__main__':
@@ -185,13 +253,22 @@ if __name__ == '__main__':
             sys.exit(1)
 
     termio = TermIO()
-    idn = join_terminal_version(*determine_terminal_version(termio))
-    if show_all:
-        id1 = join_terminal_version(*inspect_xtversion(termio))
-        id2 = join_terminal_version(*inspect_bundle_id())
-        id3 = join_terminal_version(*inspect_env_variables())
-        width = max(len(id1), len(id2), len(id3), len(idn)) + 8
-        print(f'  ansi: {id1}\nbundle: {id2}\n   env: {id3}\n{"─" * width}')
-        print(f' combo: {idn}')
-    else:
-        print(idn)
+    tn = Terminal.current()
+    if not show_all:
+        print(tn.display)
+        sys.exit(0)
+
+    l1 = l2 = l3 = ln = 0
+    t1 = Terminal.from_bundle() or Terminal.unknown()
+    l1 = len(t1.display)
+    t2 = Terminal.from_xtversion() or Terminal.unknown()
+    l2 = len(t2.display)
+    t3 = Terminal.from_env() or Terminal.unknown()
+    l3 = len(t3.display)
+
+    width = 9 + max(l1, l2, l3)
+    termio.writeln('bundle:  ' + (t1.display if t1 else ''))
+    termio.writeln('  ansi:  ' + (t2.display if t2 else ''))
+    termio.writeln('   env:  ' + (t3.display if t3 else ''))
+    termio.writeln("─" * width)
+    termio.writeln(' total:  ' + (tn.display if tn else ''))

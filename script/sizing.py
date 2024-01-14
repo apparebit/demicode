f260efe63e56bb22c3fc5b900c6a5ae16ce384b4
Robert Grimm <apparebit@gmail.com>
2024-01-13 23:22:19 -0500

    use resize_interactively()

--- script/sizing.py
+++ script/sizing.py
@@ -7,31 +7,13 @@ unnecessarily hard. This utility keeps printing the current terminal size into
 the upper left corner of the terminal, thus helping hit the target size.
 """
 
-import os
-import time
+from pathlib import Path
+import sys
 
-SLEEP = 2 / 3
+# Fix Python path to include project root
+sys.path.insert(0, str(Path.cwd()))
+
+from demicode.ui.termio import TermIO
 
 if __name__ == "__main__":
-    try:
-        # 47/1047 won't do since they don't save cursor state.
-        print("\x1b[?1049h", end=None)  # Use alternate buffer
-        print("\x1b[?25l", end=None)  # Hide cursor
-
-        block = False
-        while True:
-            # Update state
-            width, height = os.get_terminal_size()
-            block = not block
-            char = "█" if block else " "
-
-            # Display state
-            print(f"\x1b[H\x1b[2J{width:3d} x {height:3d} {char}", end=None)
-
-            # Take nap
-            time.sleep(SLEEP)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("\x1b[?25h", end=None)  # Show cursor
-        print("\x1b[?1049l", end=None)  # Use regular buffer
+    TermIO().resize_interactively(120, 40)
